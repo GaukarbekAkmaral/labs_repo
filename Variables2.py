fb83e7e28074712dfe84ebc9ed394ba4f8892010
GaukarbekAkmaral <akmaralgaukarbek2006@gmail.com>
2025-01-19 20:48:42 +0500

    Changes for Variables2.py and Variables3.py and added Variables4.py

--- Variables2.py
+++ Variables2.py
@@ -1,3 +1,21 @@
 x = 4       # x is of type int
 x = "Sally" # x is now of type str
 print(x)
+
+#Python allows you to assign values to multiple variables in one line:
+a, b, c = "Orange", "Banana", "Cherry"
+print(a)
+print(b)
+print(c)
+
+#If you have a collection of values in a list, tuple etc. Python allows you to extract the values into variables. This is called unpacking.
+fruits = ["apple", "banana", "cherry"]
+q, y, u = fruits
+print(q)
+print(y)
+print(u)
+#And you can assign the same value to multiple variables in one line:
+d = f = g = "Orange"
+print(d)
+print(f)
+print(g)
