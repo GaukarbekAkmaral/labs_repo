ddf2e79f5a456ddf393b50059804aa1bbb3602be
GaukarbekAkmaral <akmaralgaukarbek2006@gmail.com>
2025-01-22 11:52:32 +0500

    add Lab1

---
+++ Lab1/Variables3.py
@@ -0,0 +1,14 @@
+x = str(3)    # x will be '3'
+y = int(3)    # y will be 3
+z = float(3)  # z will be 3.0
+#In the print() function, you output multiple variables, separated by a comma:
+#You can also use the + operator to output multiple variables:
+a = "Python "
+b = "is "
+c = "awesome"
+print(x, y, z)
+print(a + b + c)
+#For numbers, the + character works as a mathematical operator:
+f = 5
+g = 10
+print(f + g)
---
+++ Lab1/Variables4.py
@@ -0,0 +1,22 @@
+#You can get the data type of a variable with the type() function.
+x = 5
+y = "John"
+print(type(x))
+print(type(y))
+
+#String variables can be declared either by using single or double quotes:
+z = "John"
+# is the same as
+z = 'John'
+
+a = 4
+A = "Sally"
+#A will not overwrite a
+
+#Legal variable names:
+myvar = "John"
+my_var = "John"
+_my_var = "John"
+myVar = "John"
+MYVAR = "John"
+myvar2 = "John"
